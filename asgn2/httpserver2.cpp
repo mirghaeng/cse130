4de1cee00c6ffed8f3fa1d7a4abe903e5d06dc0d
Shirley Phuong <sphuong@ucsc.edu>
2020-11-24 17:34:26 -0800

    added file stuff

--- asgn2/httpserver2.cpp
+++ asgn2/httpserver2.cpp
@@ -12,6 +12,7 @@
 #include <pthread.h>
 #include <queue>
 #include <vector>
+#include <dirent.h>
 
 #define DEFAULT_THREADS 4
 #define SIZE 500
@@ -30,10 +31,23 @@ struct shared_data {
     pthread_cond_t dispatcher_cond, worker_cond;
     int rflag;
     int sockfd;
+    int numthreads;
     struct sockaddr_in servaddr;
     std::vector<file_data> fdata;
     std::queue<sessions> session_queue;
 };
+
+const char* getStatus(int code) {
+	switch(code) {
+		case 200: return "OK";
+		case 201: return "Created";
+		case 400: return "Bad Request";
+		case 403: return "Forbidden";
+		case 404: return "Not Found";
+		case 500: return "Internal Server Error";
+	}
+	return NULL;
+}
 
 unsigned long getaddr(char *name) {
 	unsigned long res;
@@ -52,29 +66,56 @@ unsigned long getaddr(char *name) {
 	return res;
 }
 
+void sendheader(int commfd, char* response, int code, int length) {
+	sprintf(response, "\nHTTP/1.1 %d %s\r\nContent-Length: %d\r\n\r\n", code, getStatus(code), length);
+	int errno = send(commfd, response, strlen(response), 0);
+	if (errno < 0) { warn("send()"); }
+}
+
 void* dispatcher(void* data) {
     // struct
     struct shared_data* shared = (struct shared_data*) data;
+    int sockfd = shared->sockfd;
 
-    // while (1)
-        // lock mutex
-        // while (queue is full)
-            // wait
-        // accept connection
-        // put connection in queue
-        // condition signal
-        // unlock mutex
-    printf("In dispatcher\nRedundancy flag is: %d\n", shared->rflag);
-    return NULL;
+    while (1) {
+
+        pthread_mutex_lock(&shared->global_mutex);
+        while (shared->session_queue.size() >= shared->numthreads) {
+            pthread_cond_wait(&shared->dispatcher_cond, &shared->global_mutex);
+        }
+
+        char waiting[] = "\nWaiting for connection...\n";
+		write(STDOUT_FILENO, waiting, strlen(waiting));
+		int servaddr_length = sizeof(shared->servaddr);
+		int commfd = accept(sockfd, (struct sockaddr*)&(shared->servaddr), (socklen_t*)&servaddr_length);
+		if(commfd < 0) {
+			warn("accept()");
+			continue;
+		}
+
+        struct sessions request;
+        request.commfd = commfd;
+        shared->session_queue.push(request);
+        pthread_cond_signal(&shared->worker_cond);
+        pthread_mutex_unlock(&shared->global_mutex);
+    }
 }
 
 void* worker(void* data) {
     // struct
     struct shared_data* shared = (struct shared_data*) data;
 
-    // while(1)
-        // while (queue is empty)
-            // wait
+    while(1) {
+
+        // lock global mutex to get data
+        pthread_mutex_lock(&shared->global_mutex);
+        while (shared->session_queue.empty()) {
+            pthread_cond_wait(&shared->worker_cond, &shared->global_mutex);
+        }
+        int local_commfd =  shared->session_queue.front().commfd;
+        shared->session_queue.pop();
+        pthread_cond_signal(&shared->dispatcher_cond);
+        pthread_mutex_unlock(&shared->global_mutex);
         
         // parse header
         // check errors
@@ -84,8 +125,7 @@ void* worker(void* data) {
         // put
         // condition signal
         // unlock mutex
-    printf("Hello...%d\n", shared->sockfd);
-    return NULL;
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -99,11 +139,11 @@ int main(int argc, char* argv[]) {
         switch (opt) {
             case 'N':
                 num_workers = atoi(optarg);
-                printf("Option: %c\nNumber of threads: %d\n", opt, num_workers);
+                //printf("Option: %c\nNumber of threads: %d\n", opt, num_workers);
                 break;
             case 'r':
                 redundancy = 1;
-                printf("Option: %c\n", opt);
+                //printf("Option: %c\n", opt);
                 break;
         }
     }
@@ -127,7 +167,7 @@ int main(int argc, char* argv[]) {
     servaddr.sin_port = htons(port);
 
     // testing
-    printf("The address is: %s\nThe port is: %d\nThreads: %d\n", argv[optind], port, num_workers);
+    //printf("The address is: %s\nThe port is: %d\nThreads: %d\n", argv[optind], port, num_workers);
 
     // socket bind
     n = bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
@@ -137,26 +177,45 @@ int main(int argc, char* argv[]) {
     n = listen(listenfd, 10);
     if (n < 0) { err(1, "listen()"); }
 
-    // create mutexes for files and alter common_data struct
-    // go through directory make a lock for each file
-
+    // create struct to send to threads
     struct shared_data common_data;
     pthread_mutex_init(&common_data.global_mutex, NULL);
     pthread_cond_init(&common_data.dispatcher_cond, NULL);
     pthread_cond_init(&common_data.worker_cond, NULL);
     common_data.rflag = redundancy;
     common_data.sockfd = listenfd;
+    common_data.numthreads = num_workers;
     common_data.servaddr = servaddr;
-    // go through each file and make a lock
 
+    // go through each file in directory and make a lock
+    struct dirent *dp;
+    DIR *pdir;
+    pdir = opendir("./");
+    while ((dp = readdir(pdir)) != NULL) {
 
-    pthread_t dispatch_tid[SIZE], worker_tid[SIZE];
+        if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, "..")) {
+            // do nothing
+        }
+        else {
+            char* file_name = dp->d_name;
+            //printf("File: %s\n", file_name);
+
+            struct file_data filedata;
+            filedata.filename = file_name;
+            pthread_mutex_init(&filedata.file_mutex, NULL);
+            common_data.fdata.push_back(filedata);
+        }
+
+    }
+    printf("# of files: %lu\n", common_data.fdata.size());
 
     // create pthreads
+    pthread_t dispatch_tid[SIZE], worker_tid[SIZE];
+
+    pthread_create(&dispatch_tid[0], NULL, &dispatcher, &common_data);
     for (int i = 0; i < num_workers; ++i) {
         pthread_create(&worker_tid[i], NULL, &worker, &common_data);
     }
-    pthread_create(&dispatch_tid[0], NULL, &dispatcher, &common_data);
 
     sleep(10);
     pthread_mutex_destroy(&common_data.global_mutex);
