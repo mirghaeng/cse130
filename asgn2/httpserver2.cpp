22062ce75534624e53ee95589e3e789e8b902405
Shirley Phuong <sphuong@ucsc.edu>
2020-11-24 18:24:15 -0800

    this only works on the first command lul ;-;

--- asgn2/httpserver2.cpp
+++ asgn2/httpserver2.cpp
@@ -17,6 +17,9 @@
 #define DEFAULT_THREADS 4
 #define SIZE 500
 
+#define ERROR 1
+#define NO_ERROR_YET 0
+
 struct file_data {
     char* filename;
     pthread_mutex_t file_mutex;
@@ -80,9 +83,10 @@ void* dispatcher(void* data) {
     while (1) {
 
         pthread_mutex_lock(&shared->global_mutex);
-        while (shared->session_queue.size() >= shared->numthreads) {
+        while ((int)shared->session_queue.size() >= shared->numthreads) {
             pthread_cond_wait(&shared->dispatcher_cond, &shared->global_mutex);
         }
+        pthread_mutex_unlock(&shared->global_mutex);
 
         char waiting[] = "\nWaiting for connection...\n";
 		write(STDOUT_FILENO, waiting, strlen(waiting));
@@ -93,6 +97,7 @@ void* dispatcher(void* data) {
 			continue;
 		}
 
+        pthread_mutex_lock(&shared->global_mutex);
         struct sessions request;
         request.commfd = commfd;
         shared->session_queue.push(request);
@@ -104,6 +109,7 @@ void* dispatcher(void* data) {
 void* worker(void* data) {
     // struct
     struct shared_data* shared = (struct shared_data*) data;
+    struct stat st;
 
     while(1) {
 
@@ -112,19 +118,170 @@ void* worker(void* data) {
         while (shared->session_queue.empty()) {
             pthread_cond_wait(&shared->worker_cond, &shared->global_mutex);
         }
-        int local_commfd =  shared->session_queue.front().commfd;
+        int commfd =  shared->session_queue.front().commfd;
         shared->session_queue.pop();
         pthread_cond_signal(&shared->dispatcher_cond);
         pthread_mutex_unlock(&shared->global_mutex);
-        
-        // parse header
-        // check errors
 
-        // lock mutex
-        // get
-        // put
-        // condition signal
-        // unlock mutex
+        char buf[100];
+		char header[5000];
+		char headercpy[5000];
+		char response[10000];       // initialize to size of content + 10000 for the header
+		char *end_of_header, *type, *filename;
+		int errors = NO_ERROR_YET;
+        int contentlength, filesize;
+
+        while(recv(commfd, buf, sizeof(char), 0) > 0) {
+		    write(STDOUT_FILENO, buf, sizeof(char));
+		    strcat(header, buf);
+		    end_of_header = strstr(header, "\r\n\r\n");
+		    if(end_of_header != NULL) { break; }
+			memset(&buf, 0, sizeof(buf));
+		}
+
+        // get request type
+		strcpy(headercpy, header);
+		type = strtok(headercpy, " ");
+		
+		// get filename
+		filename = strtok(NULL, " ");
+		filename++;
+		
+		// check filename length
+		if(strlen(filename) != 10) {
+			// 400 Bad Request
+			errors = ERROR;
+			sendheader(commfd, response, 400, 0);
+		}
+
+        // check filename is alphanumeric
+		for(char* i = filename; *i != '\0'; i++) {
+			if((isalnum(*i) == 0) && (errors == NO_ERROR_YET)) {
+
+				// 400 Bad Request
+				errors = ERROR;
+				sendheader(commfd, response, 400, 0);
+			}
+		}
+
+		// check for HTTP/1.1
+		char* ptrhttp = strstr(header, "HTTP/1.1");
+		if((ptrhttp == NULL) && (errors == NO_ERROR_YET)) {
+
+			// 400 Bad Request
+			errors = ERROR;
+			sendheader(commfd, response, 400, 0);
+		}
+
+		// check for other HTTP/1.1 methods
+		if (((strcmp(type, "GET") != 0) && (strcmp(type, "PUT") != 0)) && (errors == NO_ERROR_YET)) {
+
+			// 500 Internal Service Error
+			errors = ERROR;
+			sendheader(commfd, response, 500, 0);
+        }
+
+        // handling GET request
+        int getfd;	
+		memset(&buf, 0, sizeof(buf));
+		if((strcmp(type, "GET") == 0) && (errors == NO_ERROR_YET)) {
+			if(access(filename, F_OK) == -1) {
+
+				// 404 File Not Found
+				errors = ERROR;
+				sendheader(commfd, response, 404, 0);
+			} else if(access(filename, R_OK) == -1) {
+
+				// 403 Forbidden
+				errors = ERROR;
+				sendheader(commfd, response, 403, 0);
+			} else {
+
+				// get file size
+				stat(filename, &st);
+				filesize = st.st_size;
+
+                char *responseGet;
+                responseGet = new char[filesize+10000];
+
+				sendheader(commfd, responseGet, 200, filesize);
+
+				// get file contents
+				getfd = open(filename, O_RDONLY);
+				while(read(getfd, buf, sizeof(char))) {
+					send(commfd, buf, sizeof(char), 0);
+					memset(&buf, 0, sizeof(buf));
+				}
+				close(getfd);
+
+                delete[] responseGet;
+			}
+		}		
+		
+		// handling PUT request
+        int putfd, n;
+		memset(&buf, 0, sizeof(buf));
+		if((strcmp(type, "PUT") == 0) && (errors == NO_ERROR_YET)) {
+            
+			// get Content-Length
+			char* ptrlength = strstr(header, "Content-Length:");
+
+			if(ptrlength != NULL) {
+
+				// get content if Content-Length is provided
+				sscanf(ptrlength, "Content-Length: %d", &contentlength);
+
+            	char *responsePut = new char[contentlength+10000];
+
+				putfd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+				for(int i = contentlength; i > 0; i--) {
+					n = recv(commfd, buf, sizeof(char), 0);
+					if(n < 0) { warn("recv()"); }
+					if(n == 0) { break; }
+					write(STDOUT_FILENO, buf, sizeof(char));
+					write(putfd, buf, sizeof(char));
+					memset(&buf, 0, sizeof(buf));
+				}
+
+				close(putfd);
+
+				sendheader(commfd, responsePut, 201, 0);
+				delete[] responsePut;
+
+			} else {
+				
+				// // get content if Content-Length is not provided
+
+                char *responsePut = new char[20000];
+
+				putfd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+				sendheader(commfd, responsePut, 201, 0);
+
+				while (1) {
+					int amtRcv = recv(commfd, buf, sizeof(char), 0);
+					if (amtRcv == 0) {
+						break;
+					}
+					write(STDOUT_FILENO, buf, sizeof(char));
+					write(putfd, buf, sizeof(char));
+					memset(&buf, 0, sizeof(buf));
+				}
+
+				close(putfd);
+
+				delete[] responsePut;
+
+			}
+		}
+
+		// clear buffers
+		memset(&header, 0, sizeof(header));
+		memset(&headercpy, 0, sizeof(headercpy));
+		memset(&response, 0, sizeof(response));
+		
+		// close TCP connection
+		close(commfd);
     }
 }
 
